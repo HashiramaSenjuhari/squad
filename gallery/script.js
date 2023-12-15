0e2bdc22b62602bc74883abaa3e3c81fdc6784f6
Hariprasath <truebillionhari@gmail.com>
2023-12-15 15:02:12 +0530

    df

---
+++ gallery/script.js
@@ -0,0 +1,16 @@
+const image = document.querySelector('.hero img');
+
+const reset = () => image.classList.remove('fadeIn');
+
+function activate(e) {
+  if (!e.target.matches('.btns img')) return;
+  image.src = e.target.src;
+  image.classList.add('fadeIn');
+}
+
+document.addEventListener('click',activate,false);
+document.addEventListener('animationend',reset,false);
+
+document.addEventListener('DOMContentLoaded',() =>{
+  document.body.style.opacity = '1'
+})
